53f89ca4b4904778f2b5abd9b61fb073a61d9537
PM Extra <pm@jubeat.net>
2020-02-13 17:41:30 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,29 @@
+# Asp.Net One rad tool for vscode 
+
+## Description
+
+aspnet one rad tool is a VS Code extension which lets you create an entity and its corresponding layers from the database to the user interface. So, you can create a fully working, production ready CRUD page in seconds.
+
+It is similar with the official visual studio extension: [ASP.NET One Power Tools](https://marketplace.visualstudio.com/items?itemName=Volosoft.AspNetOnePowerTools)
+
+![example](images/example.gif)
+
+## Usage
+
+1. new a YourEntity.json file in the `aspnet-core\AspNetOneRadTool` folder
+2. using code snippets to edit YourEntity.json file
+3. run the command `Run ASP.NET One Rad Tool` from explore context menu
+
+
+## Available Snippets
+
+* one-entity
+* one-property
+* one-nav-property
+* one-enum-def
+
+## Fields of entity json
+
+please refs aspnet one document [https://docs.aspnetone.com/documents/one/latest/Development-Guide-Rad-Tool-Mac-Linux](https://docs.aspnetone.com/documents/one/latest/Development-Guide-Rad-Tool-Mac-Linux)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,50 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import * as path from "path";
+import { spawn } from "child_process";
+import * as AdmZip from "adm-zip";
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext): void {
+  const commandOutput = vscode.window.createOutputChannel("Shell");
+  context.subscriptions.push(commandOutput);
+
+  const disposable = vscode.commands.registerCommand(
+    "extension.runRadTool",
+    (uri: vscode.Uri) => {
+      commandOutput.show();
+      commandOutput.appendLine("-> code generation is begining...");
+
+      // unzip AspNetOneRadTool.zip to folder
+      const zip = new AdmZip(
+        path.join(__dirname, "..", "resources/AspNetOneRadTool.zip")
+      );
+      zip.extractAllTo(
+        /*target path*/ path.dirname(uri.fsPath),
+        /*overwrite*/ true
+      );
+
+      // run rad tool
+      const process = spawn(
+        "dotnet.exe",
+        ["AspNetOneRadTool.dll", path.basename(uri.fsPath)],
+        { cwd: path.dirname(uri.fsPath), shell: true }
+      );
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      function printOutput(data: any): void {
+        commandOutput.append(data.toString());
+        process.stdin.write("\x0D");
+      }
+      process.stdout.on("data", printOutput);
+      process.stderr.on("data", printOutput);
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate(): void {
+  // this method is called when your extension is deactivated
+}
